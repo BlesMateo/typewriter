cb1c00cc54ce74dd20e6ea04ddc9ed64732d241b
BlesMateo <blesildaangelicamateo@gmail.com>
2021-11-02 14:54:48 -0400

    Make a sentence and display it as an animation

---
+++ typewriter.js
@@ -0,0 +1,14 @@
+let typeWriter = function() {
+  const sentence = "I really want a beef taco with cilantro today";
+  let timer = 0;
+  let count = 0;
+  for (const char of sentence) {
+    count = count + 1; //increment value of count by 1
+    timer = timer + 50; // 'I' = 0 ms, 'r' = 50 ms // timer increments by 50 ms
+    setTimeout(() => {
+    process.stdout.write(char);
+    }, timer);
+  }
+}
+
+typeWriter();
